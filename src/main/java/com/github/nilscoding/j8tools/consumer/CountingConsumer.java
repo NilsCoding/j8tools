0c33475f90e746c73611f990241f0f515016d4ce
NilsCoding <nils.althof+github@googlemail.com>
2015-05-26 21:30:10 +0200

    added counting consumer

---
+++ src/main/java/com/github/nilscoding/j8tools/consumer/CountingConsumer.java
@@ -0,0 +1,39 @@
+package com.github.nilscoding.j8tools.consumer;
+
+import java.util.function.Consumer;
+
+/**
+ * Consumer which counts how many elements have been consumed and forwards them to another consumer
+ * @author NilsCoding
+ * @param <T>   type of consumer
+ */
+public class CountingConsumer<T> implements Consumer<T> {
+
+    protected Consumer<T> parentConsumer;
+    protected long count;
+    
+    /**
+     * Creates a counting consumer
+     * @param parent    parent consumer to forward to
+     */
+    public CountingConsumer(Consumer<T> parent) {
+        this.parentConsumer = parent;
+    }
+
+    /**
+     * Returns the number of consumed elements
+     * @return  number of consumed elements
+     */
+    public long getCount() {
+        return this.count;
+    }
+    
+    @Override
+    public void accept(T t) {
+        this.count++;
+        if (this.parentConsumer != null) {
+            this.parentConsumer.accept(t);
+        }
+    }
+    
+}
